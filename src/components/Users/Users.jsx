a4efa80e67cffb5b2939e996aaa24fbc8f051ef6
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-21 19:55:30 +0600

    import useLoaderData & set users length

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,17 @@
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    //state--> data
-    //state-->loader
-    //use effect 
-    //fetch --> state set -->set state
-    return (
-        <div>
-<h2>Our Users</h2>            
-        </div>
-    );
+  const users = useLoaderData();
+  //state--> data
+  //state-->loader
+  //use effect
+  //fetch --> state set -->set state
+  return (
+    <div>
+      <h2>Our Users:{users.length}</h2>
+      <p>All users are here.</p>
+    </div>
+  );
 };
 
 export default Users;

674884d7a9549595d9ecc8b6d7e91d54471b002f
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-21 17:14:34 +0600

    use loader fetch

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,5 +1,9 @@
 
 const Users = () => {
+    //state--> data
+    //state-->loader
+    //use effect 
+    //fetch --> state set -->set state
     return (
         <div>
 <h2>Our Users</h2>            

5b8eb7c70e56a8a66d98c0348b19bcf49bcad7c4
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-25 23:55:51 +0600

    error page setup

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,15 @@
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-<h2>Oops!!!</h2>            
-        </div>
-    );
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div>
+      <h2>Oops!!!</h2>
+      <p>{error.statusText || error.message}</p>
+    </div>
+  );
 };
 
 export default ErrorPage;

1b857d98473d9b0c00ff7c3b48a780faff3f1a34
Alal Hosen <alalhosen1992620120@gmail.com>
2025-10-28 16:38:56 +0600

    last change some

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,17 +3,17 @@ import { Link, useNavigate } from "react-router-dom";
 const Post = ({ post }) => {
   const { title, id, userId, body } = post;
 
-const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const postStyle = {
     border: "3px solid yellow",
     padding: "5px",
     borderRadius: "20px",
-  };
+  }; 
 
-const handleShowDetail=()=>{
-navigate(`/post/${id}`);
-}
+  const handleShowDetail = () => {
+    navigate(`/post/${id}`);
+  };
 
   return (
     <div style={postStyle}>
@@ -22,7 +22,9 @@ navigate(`/post/${id}`);
       <p>{userId}</p>
       <p>{body}</p>
       <Link to={`/post/${id}`}>Post Detail</Link>
-      <Link to={`/post/${id}`}><button>Show Details</button></Link>
+      <Link to={`/post/${id}`}>
+        <button>Show Details</button>
+      </Link>
       <button onClick={handleShowDetail}>Click to see details</button>
     </div>
   );
